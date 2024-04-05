01098de3d067781a0c804efab3b188d5b3fc279a
anyway93 <lucky_duky@mail.ru>
2024-04-06 00:09:32 +0300

    second

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import Slider from "react-slick";
-
 import '../styles/mainpage/mainpage.css';
 import '../styles/component/card/card.css';
 import { Card } from '../components/Card';
-
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-
-
 
 function Mainpage() {
   return (
